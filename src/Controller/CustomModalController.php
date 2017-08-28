601abad65ba1533c18cba6b8cd768c61359a7f86
Blair Wadman <blair@wadmanconsulting.com>
2017-08-28 11:03:18 +0100

    Added non-JS version

--- src/Controller/CustomModalController.php
+++ src/Controller/CustomModalController.php
@@ -10,16 +10,23 @@ namespace Drupal\custom_modal\Controller;
 use Drupal\Core\Ajax\AjaxResponse;
 use Drupal\Core\Ajax\OpenModalDialogCommand;
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\Response;
 
 class CustomModalController extends ControllerBase {
 
-  public function modal() {
-    $options = [
-      'dialogClass' => 'popup-dialog-class',
-      'width' => '50%',
-    ];
-    $response = new AjaxResponse();
-    $response->addCommand(new OpenModalDialogCommand(t('Modal title'), t('The modal text'), $options));
+  public function modal($js = 'nojs') {
+    if ($js == 'ajax') {
+      $options = [
+        'dialogClass' => 'popup-dialog-class',
+        'width' => '50%',
+      ];
+      $response = new AjaxResponse();
+      $response->addCommand(new OpenModalDialogCommand(t('Modal title'), t('The modal text'), $options));
+    }
+    else {
+      $response = new Response();
+      $response->setContent('This is an example of a fallback for a modal without Javascript.');
+    }
 
     return $response;
   }
